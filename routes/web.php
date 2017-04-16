91bbf1dda9bf1a04d05fcf27cef98f902715bae1
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-17 06:57:16 +0700

    Generate scaffolding auth

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
