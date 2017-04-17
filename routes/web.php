23a449545a185a1738af48c48570f83c4cb4e537
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-18 01:03:03 +0700

    Update route reset password

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@s
 Route::post('register', ['as' => 'register.post', 'uses' => 'Auth\RegisterController@register']);
 
 // Password Reset Routes...
-Route::get('password/reset', ['as' => 'password.reset', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+Route::get('password/reset', ['as' => 'password.request', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
 Route::post('password/email', ['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
-Route::get('password/reset/{token}', ['as' => 'password.reset.token', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
-Route::post('password/reset', ['as' => 'password.reset.post', 'uses' => 'Auth\ResetPasswordController@reset']);
+Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+Route::post('password/reset', ['uses' => 'Auth\ResetPasswordController@reset']);
