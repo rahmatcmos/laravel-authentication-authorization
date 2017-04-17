ce23c05f72904de6d6495f85de0a633d8c05d66e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-17 07:05:56 +0700

    Delete route

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,6 @@ Route::get('/home', function() {
     return "Anda berhasil login";
 });
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
