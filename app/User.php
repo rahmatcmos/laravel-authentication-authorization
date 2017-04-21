88c3c2ac048aeee3a1a87f708c7b2540a7433d62
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-22 03:02:12 +0700

    Add role mass asignment

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'username', 'email', 'password', 'provider', 'provider_id'
+        'name', 'username', 'email', 'password', 'provider', 'provider_id', 'role'
     ];
 
     /**
