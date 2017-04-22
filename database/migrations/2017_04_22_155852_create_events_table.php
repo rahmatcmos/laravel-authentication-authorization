5cd872dd1306cc8d348b6eb998f29ab8130520c6
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-22 23:12:12 +0700

    Create events migration

---
+++ database/migrations/2017_04_22_155852_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('organizer_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('location');
+            $table->date('begin_date');
+            $table->date('finish_date');
+            $table->boolean('published')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
