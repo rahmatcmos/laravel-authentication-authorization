6356c4ce762ed733510d241e7b24ac5b1a309e93
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-22 21:56:13 +0700

    Create settings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Gate;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
         return view('home');
+    }
+
+    public function settings()
+    {
+        if (Gate::allows('be-organizer')) {
+          return view('settings.organizer');
+        }
+
+        if (Gate::allows('be-participant')) {
+          return view('settings.participant');
+        }
     }
 }
