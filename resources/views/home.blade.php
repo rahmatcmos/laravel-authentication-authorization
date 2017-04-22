999b54b76f674717ff85d913ba946583f4a7d719
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-22 21:57:20 +0700

    Create view gate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,3 +3,10 @@
 @endif
 
 <p>Selamat datang {{ Auth::user()->name }}</p>
+
+@can('be-organizer')
+  <p><a href="/event">Event</a></p>
+@endcan
+@can('be-participant')
+  <p><a href="/event-history">Event History</a></p>
+@endcan
