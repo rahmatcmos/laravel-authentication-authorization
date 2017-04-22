ad568fe864a851c5e80ee5d0b3567fe5ec432e01
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-23 00:49:17 +0700

    Add jwt auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'role' => \App\Http\Middleware\HasRole::class,
+        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
     ];
 }
