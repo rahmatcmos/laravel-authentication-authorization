1f9fc352d8df7367d95b456f3459851f84e887bd
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-22 23:13:13 +0700

    Add all event view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,3 +10,13 @@
 @can('be-participant')
   <p><a href="/event-history">Event History</a></p>
 @endcan
+
+<h3>Semua Event</h3>
+@foreach (App\Event::get() as $event)
+  <p><strong>Event: {{ $event->name }}</strong></p>
+  <p>{{ $event->description }}</p>
+  <p>Tempat/Waktu: {{ $event->location }}, {{ $event->begin_date }} - {{ $event->finish_date }}</p>
+@can ('be-organizer')
+  <a href="/edit-event/{{ $event->id }}">Edit Event</a>
+@endcan
+@endforeach
