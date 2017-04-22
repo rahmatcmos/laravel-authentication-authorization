4e8c3b3b5cee68389064dbc9216501d31e4c0982
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-04-23 00:49:09 +0700

    Generate JWT key

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'RAqOyB292PsWzmH0gZh0xcHg4Cdhj6zo'),
 
     /*
     |--------------------------------------------------------------------------
